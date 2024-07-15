911ebc7f87235e5deb00bd29b2cd8723accd1c35
luisc <luisclarosbazoalto@gmail.com>
2024-07-15 18:09:36 -0400

    define una función def_hundler que maneja la señal de interrupción SIGINT (Ctrl+C). Cuando se recibe esta señal, imprime un mensaje y sale del programa.

--- fuizz.py
+++ fuizz.py
@@ -4,3 +4,8 @@ import signal
 import requests
 from tqdm import tqdm
 from termcolor import colored
+
+def def_hundler(sig, frame):
+    print(colored("[!] Saliendo del programa...", 'red'))
+    sys.exit(1)
+signal.signal(signal.SIGINT, def_hundler)
